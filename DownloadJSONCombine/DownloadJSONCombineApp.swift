7297cc4342040bcbff75bad014831712559bf2ba
superkookai <weerawut@live.com>
2025-03-13 09:28:36 +0700

    Initial Commit

---
+++ DownloadJSONCombine/DownloadJSONCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadJSONCombineApp.swift
+//  DownloadJSONCombine
+//
+//  Created by Weerawut Chaiyasomboon on 13/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct DownloadJSONCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
